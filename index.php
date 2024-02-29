011b787f6a6c3ef86c67db942223d3e78bd1442d
opsecx <108930243+opsecx@users.noreply.github.com>
2024-02-29 10:23:14 +0100

    update to v2

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Testnet.one Governance</title></head>
+
+
+<body>
+<h2>Testnet.one Governance Aid Tools</h2>
+
+A small collection of governance tools:
+
+<ul>
+<li><a href="check.php">Simple Votes Checker v1 (original)</a></li>
+<li><a href="check_v2.php">Simple Votes Checker v2 (more functions)</a></li>
+
+
+</ul>
+
+
+</body>
+</html>
