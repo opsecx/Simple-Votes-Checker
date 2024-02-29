ec917565d28290bb946fc55dba92a374b262e5ff
opsecx <applicationfile@protonmail.com>
2024-02-29 11:15:08 +0100

    missed files

--- functions423.php
+++ functions423.php
@@ -9,13 +9,46 @@ function strip_all($in) {
 }
 
 function validate_tnam1($in) {
+  if (empty($in)) {
+    return false;
+  }    
   strip_all($in);	
   if (strlen($in) <> 45) {return false;}	
   $reg_tnam1 = '/tnam1[a-zA-Z0-9]{40}/i';	
   return preg_match($reg_tnam1, $in);
 }
 
+function validate_proposal_id($in) {
+  if (empty($in)) {
+    return false;
+  }    
+  strip_all($in);
+  if (strlen($in) > 25) {return false;}
+  $reg_tnam1 = '/\d+/i';
+  return preg_match($reg_tnam1, $in);
+}
 
+
+function hyperlink_address($in, $mode_arg) {
+   $in = strip_all($in);	
+   $mode_arg = strip_all($mode_arg);
+   if (validate_tnam1($in) == false) {die('wrong internal parameter call');}
+  $uri = strip_all($_SERVER['REQUEST_URI']);
+ $uri = array_shift(explode('?', $uri)); 
+  $servername = strip_all($_SERVER['HTTP_HOST']);
+  $newlink = 'https://' . $servername . $uri . '?address=' . $in . '&mode=' . $mode_arg . '&proposal_id=';
+  $returnstring = '<a href="' . $newlink . '">' . $in . '</a>'; 
+  return $returnstring;
+}
+
+function hyperlink_proposal($in, $mode_arg) {
+  $uri = strip_all($_SERVER['REQUEST_URI']);
+ $uri = array_shift(explode('?', $uri));
+  $servername = strip_all($_SERVER['HTTP_HOST']);
+  $newlink = 'https://' . $servername . $uri . '?proposal_id=' . $in;
+  $returnstring = '<a href="' . $newlink . '">' . $in . '</a>';
+  return $returnstring;
+}
 
 
 
