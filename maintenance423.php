67f63eb441b7bb6f36e0f9ce4851eb63b4fa8ba8
opsecx <108930243+opsecx@users.noreply.github.com>
2024-03-07 13:36:59 +0100

    Removed current maintenance note

--- maintenance423.php
+++ maintenance423.php
@@ -1,6 +1,6 @@
 <?php 
 
-$maintenance = "03/07: Network will resume today, and this service may be somewhat unstable and/or display outdated data while we sync. Please be mindful, we will remove this notice once up to date";
+$maintenance = "";
 
 
 
