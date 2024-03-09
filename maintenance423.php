58141228418e5bb9f86c14e113f73ea045539c3d
opsecx <108930243+opsecx@users.noreply.github.com>
2024-03-09 20:06:09 +0100

    Added latest block count

--- maintenance423.php
+++ maintenance423.php
@@ -2,6 +2,15 @@
 
 $maintenance = "";
 
+$query_m = "select max(header_height) as latest_block from shielded_expedition.blocks";
 
+$results_m = pg_query($dbconn, $query_m)
+	or die('could not fetch latest indexed block number');
+
+$latest_indexed_block = pg_fetch_array($results_m, null, PGSQL_NUM)[0];
+
+$maintenance = $maintenance . " The tools are synced up to block height $latest_indexed_block. Please check this corresponds to latest block height.";
+
+pg_free_result($results_m);
 
 ?>
